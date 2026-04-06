bb01be85fbca95281df6f9a1533f5c86779b5533
Aizhouym <3551696833@qq.com>
2026-04-06 10:48:16 -0400

    Upload Cedar Forge project content

---
+++ src/test_lm.py
@@ -0,0 +1,36 @@
+
+from openai import OpenAI
+
+
+qwen9b_url = "http://:8000/v1"
+qwen27b_url = "http://g202:8001/v1"
+qwen35b_url = "http://g201:8002/v1"
+
+qwen9b = "qwen9b"
+qwen27b = 'qwen27b'
+qwen35b = 'qwen35b'
+
+client = OpenAI(
+    base_url=qwen27b_url,
+    api_key="EMPTY"
+)
+
+response = client.chat.completions.create(
+    model=qwen27b,
+    messages=[
+        {
+            "role": "user",
+            "content": "Can you tell me what is the cedar language(policy language for access control policy)? simply explain your thought."
+        }
+    ],
+    temperature=0,
+    max_tokens=1024,
+    extra_body={
+        "chat_template_kwargs": {
+            "enable_thinking": False,
+        }
+    },
+)
+
+print(response.choices[0].message.content.strip())
+
